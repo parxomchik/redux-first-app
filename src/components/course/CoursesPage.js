51b36c7100c9d04864a5ec533614e5b1d742eb35
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-07-09 22:25:48 +0300

    added courses comp

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+export default class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}

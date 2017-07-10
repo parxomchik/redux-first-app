d30c433807e29b3f3f02d4083e72eb9f4dab509d
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-07-10 12:00:20 +0300

    added store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,1 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReduser from '../redusers/index';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReduser,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
